63641efa98e3a0658c8c27deb89e002a335a5ed9
jiangchao <jiangchaobj@yahoo.com>
2016-03-16 23:57:57 +0800

    添加注册页面

--- app/src/main/java/com/example/jc/personalaccount/LoginActivity.java
+++ app/src/main/java/com/example/jc/personalaccount/LoginActivity.java
@@ -98,6 +98,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
+        Button mRegisterButton = (Button) findViewById(R.id.regist_button);
+        mRegisterButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_register);
+            }
+        });
+
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
 
